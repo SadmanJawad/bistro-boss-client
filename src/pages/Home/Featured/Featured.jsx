afd12240050ec3a03c93f904c8e06cebef276bcd
SadmanJawad <jawadnabil977@gmail.com>
2023-05-24 02:08:47 +0600

    Featured items section

---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,34 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import featuredImg from "../../../assets/home/featured.jpg";
+import './Featured.css'
+
+const Featured = () => {
+  return (
+    <div className="featured-item text-white pt-8 my-20">
+      <SectionTitle
+        subHeading="Check it out"
+        heading="Featured Items"
+      ></SectionTitle>
+      <div className="md:flex justify-center items-center pb-20 pt-12 px-36" >
+        <div>
+          <img src={featuredImg} alt="" />
+        </div>
+        <div className="md:ml-10">
+          <p>Aug 20, 2023</p>
+          <p className="uppercase">Where can I get some?</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi beatae
+            iste sapiente incidunt recusandae. Laborum distinctio eveniet
+            consectetur! Nostrum, eveniet. Repudiandae cumque modi odit ex,
+            illum vel, enim voluptas est, natus deleniti error autem
+            consequuntur! Cumque eum omnis delectus reiciendis a. Commodi nihil
+            recusandae dicta iste vitae eaque, aut impedit.
+          </p>
+          <button className="btn btn-outline">Order Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
