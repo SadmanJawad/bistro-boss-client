8d6cc6f3ee26bb478f4a4d0029ffc611a5d56e75
SadmanJawad <jawadnabil977@gmail.com>
2023-05-28 12:10:55 +0600

    Firebase Project Setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/Routes.jsx'
-import { HelmetProvider } from 'react-helmet-async';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Routes.jsx";
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./providers/AuthProvider";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <HelmetProvider>
-  <div className='max-w-screen-xl mx-auto'><RouterProvider router={router} /></div>
-  </HelmetProvider>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="max-w-screen-xl mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
