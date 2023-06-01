08510b10c7ec64717c0dab50a9c0f0e83afa6ca9
SadmanJawad <jawadnabil977@gmail.com>
2023-06-01 16:53:58 +0600

    role button error fixed

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,18 +1,18 @@
+import { useQuery } from '@tanstack/react-query'
 import { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
-import { useQuery } from '@tanstack/react-query';
-
 const useCart = () => {
-     const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
 
-     const { refetch, data: cart = [] } = useQuery({
+    const { refetch, data: cart = [] } = useQuery({
         queryKey: ['carts', user?.email],
-        queryFn: async() => {
-            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`);
-        return response.json();
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+            return res.json();
         },
-      })
+    })
 
-      return[cart, refetch]
+    return [cart, refetch]
+
 }
 export default useCart;
--- src/pages/DashBoard/AllUsers/AllUsers.jsx
+++ src/pages/DashBoard/AllUsers/AllUsers.jsx
@@ -1,15 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
-  const { data: users = [], refetch } = useQuery(["users"], async () => {
+  const { data: users = [], refetch } = useQuery(['users'], async () => {
     const res = await fetch("http://localhost:5000/users");
     return res.json();
   });
 
-  const handleMakeAdmin = id => {
-    
+  const handleMakeAdmin = user => {
+    fetch(`http://localhost:5000/users/admin/${user._id}`, {
+        method: 'PATCH',
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        if(data.modifiedCount){
+            refetch();
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: `${user.name} is now an admin`,
+                showConfirmButton: false,
+                timer: 1500
+              })
+        }
+    })
   }
 
     const handleDelete = (user) => {
@@ -43,7 +60,7 @@ const AllUsers = () => {
             <td>{user.name}</td>
             <td>{user.email}</td>
             <td>{user.role === 'admin' ? 'admin' : 
-            <button onClick={() => handleMakeAdmin(user._id)} className="btn btn-ghost text-white bg-orange-600"><FaUserShield/></button>
+            <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost text-white bg-orange-600"><FaUserShield/></button>
             
                 }</td>
             <td>   <button onClick={() => handleDelete(user)} className="btn btn-ghost text-white bg-red-600"><FaTrashAlt/></button></td>
