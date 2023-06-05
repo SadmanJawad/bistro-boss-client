642b23eee37326bf4f99a26ba02778d711c9c028
SadmanJawad <jawadnabil977@gmail.com>
2023-06-05 23:56:25 +0600

    add stripe and use publishable key pk

--- src/pages/DashBoard/Payment/Payment.jsx
+++ src/pages/DashBoard/Payment/Payment.jsx
@@ -1,10 +1,18 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
+//TODO: add stripe public key
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK)
 const Payment = () => {
     return (
         <div>
             <SectionTitle subHeading='please process' heading='Payment'></SectionTitle>
-            <h2 className="text-3xl">Prochur tk kamabo!!!!!!!!!!!!!!!!!!!</h2>            
+            <h2 className="text-3xl">Prochur tk kamabo!!!!!!!!!!!!!!!!!!!</h2>    
+            <Elements stripe={stripePromise}>
+                <CheckoutForm/>
+                </Elements>        
         </div>
     );
 };
